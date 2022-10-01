e6be604d1d53b4ffea05af0d89eda99818492e3d
Jay.Kishan <jay.kishan@skillassure.com>
2022-10-01 17:56:18 +0530

    added date filter for close stock

--- InventoryStockService/src/main/java/com/ros/inventory/Repository/CloseStockRepository.java
+++ InventoryStockService/src/main/java/com/ros/inventory/Repository/CloseStockRepository.java
@@ -1,10 +1,13 @@
 package com.ros.inventory.Repository;
 
 import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ros.inventory.entities.CloseStock;
@@ -14,5 +17,7 @@ public interface CloseStockRepository extends JpaRepository<CloseStock,UUID> {
 
    	@Query(value="SELECT * FROM close_stock ORDER BY stock_end_date DESC limit 1",nativeQuery=true)
 	public CloseStock OpeningDate();
+   	@Query(value = "SELECT * from close_stock where stock_start_date >= :start_date and stock_start_date <= :end_date",nativeQuery = true)
+    public List<CloseStock> getStockPeriod(@Param("start_date") Date start_date, @Param("end_date") Date end_date );
 
 }
--- InventoryStockService/src/main/java/com/ros/inventory/service/IClosingValueService.java
+++ InventoryStockService/src/main/java/com/ros/inventory/service/IClosingValueService.java
@@ -1,5 +1,6 @@
 package com.ros.inventory.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.util.MultiValueMap;
@@ -7,11 +8,14 @@ import org.springframework.util.MultiValueMap;
 import com.ros.inventory.Exception.InventoryException;
 import com.ros.inventory.controller.dto.CloseStockDto;
 import com.ros.inventory.controller.dto.ClosingValueDto;
+import com.ros.inventory.entities.CloseStock;
 
 public interface IClosingValueService {
 
 	List<ClosingValueDto> getValues() throws InventoryException;
 
 	String  setValues(List<ClosingValueDto> close_stock_values) throws InventoryException;
+
+    List<CloseStock> getStockPeriod(Date start_Date, Date end_date) throws InventoryException;
 
 }
